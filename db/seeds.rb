c6b06c146297663f8722e188bde1d99de1b4d45b
Matt Lins <mattlins@gmail.com>
2023-05-05 22:43:07 -0500

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  cage_one = Cage.create!(power_status: "active")
+  Dinosaur.create!(name: "Jake", species: "Tyrannosaurus", cage: cage_one)
+  Dinosaur.create!(name: "Sally", species: "Tyrannosaurus", cage: cage_one)
+  Dinosaur.create!(name: "Fred",  species: "Tyrannosaurus", cage: cage_one)
+
+  cage_two = Cage.create!(power_status: "active")
+  Dinosaur.create!(name: "George", species: "Velociraptor", cage: cage_two)
+  Dinosaur.create!(name: "Jane",  species: "Velociraptor", cage: cage_two)
+  Dinosaur.create!(name: "Sue",   species: "Velociraptor", cage: cage_two)
+
+  cage_three = Cage.create!(power_status: "active")
+  Dinosaur.create!(name: "Jim",   species: "Stegosaurus", cage: cage_three)
+  Dinosaur.create!(name: "Bob",   species: "Brachiosaurus", cage: cage_three)
+  Dinosaur.create!(name: "Erin", species: "Triceratops", cage: cage_three)
+
+  cage_four = Cage.create!(power_status: "down")
+end
